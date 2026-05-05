eaffd83145c4388d9153905c72964793138c2608
KrishnaveniKovoor <krish.kovoor1509@gmail.com>
2026-05-05 12:44:07 +0530

    Added week 1 assignments
    Added week 1 assignments

---
+++ Week-1/assgn3.js
@@ -0,0 +1,11 @@
+//2 find the big no in the given three no's
+let a=10;
+let b=100;
+let c=300;
+if (a>b && a>c) {
+console.log("a is biggest");
+} else if (b>a && b>c) {
+  console.log("b is biggest");
+} else {
+  console.log("c is biggest");
+}
